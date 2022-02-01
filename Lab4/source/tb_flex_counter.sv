0077f1b9fda81f6ec1d0cc1beef881647fd7498d
Dorien Penebacker <dpenebac@purdue.edu>
2022-02-01 17:35:11 -0500

    slowly but surely

--- Lab4/source/tb_flex_counter.sv
+++ Lab4/source/tb_flex_counter.sv
@@ -13,7 +13,9 @@ module tb_flex_counter
 
     localparam NUM_CNT_BITS = 5;
 
-    localparam CLK_PERIOD = 1;
+    localparam  CLK_PERIOD    = 1;
+    localparam  FF_SETUP_TIME = 0.190;
+    localparam  FF_HOLD_TIME  = 0.100;
 
     integer test_num;
     string test_case;
@@ -41,29 +43,30 @@ module tb_flex_counter
 
     //Normal Operation Check
     task checkOutput;
-        input logic val;
+        input logic out, flag;
         input string check;
     begin
-        if (val == tb_count_out) begin
+        if (out == tb_count_out) begin
             $display("Count out is good for: %s", check);
         end
         else begin
             $display("Count out is not good for: %s", check);
         end
 
-        if (val == tb_rollover_flag) begin
-            $display("Rollover is good for: %s", check);
+        if (flag == tb_rollover_flag) begin
+            $display("Rollover Flag is good for: %s", check);
         end
         else begin
-            $display("Rollover is not good for: %s", check);
+            $display("Rollover Flag is not good for: %s", check);
         end
     end
+    endtask
 
     //Generate clock
     always begin
-        clk = 1'b0;
+        tb_clk = 1'b0;
         #(CLK_PERIOD/2.0); //wait .5 ns
-        clk = 1'b1;
+        tb_clk = 1'b1;
         #(CLK_PERIOD/2.0);
     end
 
@@ -77,6 +80,8 @@ module tb_flex_counter
         //initialize test inputs
         tb_n_rst = 1'b1;
         tb_count_enable = 1'b0;
+        tb_clear = 1'b0;
+
         test_case = "Initialization";
         test_num = 0;
 
@@ -88,9 +93,28 @@ module tb_flex_counter
         
         #(0.1);
 
+        tb_count_enable = 1'b0;
+        tb_n_rst = 1'b0; //turn on reset
 
+        #(CLK_PERIOD * 0.5); //wait
+
+        checkOutput(0, 0, "(Checking after turning on reset)"); //output and flag should be 0
+
+        #(CLK_PERIOD); //wait
+
+        checkOutput(0, 0, "(Checking after 1 clock cycle after turning on reset)");
+
+        @(posedge tb_clk)
+        #(2 * FF_HOLD_TIME); //wait
+
+        tb_n_rst = 1'b1; //turn off reset
+        #0.1; //wait
+
+        checkOutput(0, 0, "(Checking after reset was turned off)");
 
         //Test 2
+        test_case = "Continous Counting";
+        test_num = 2;
 
     end
 
---
+++ Lab4/wave.do
@@ -0,0 +1,29 @@
+onerror {resume}
+quietly WaveActivateNextPane {} 0
+add wave -noupdate -expand -group Tests /tb_flex_counter/test_num
+add wave -noupdate -expand -group Tests /tb_flex_counter/test_case
+add wave -noupdate -expand -group Inputs -label Clk /tb_flex_counter/tb_clk
+add wave -noupdate -expand -group Inputs -label Reset /tb_flex_counter/tb_n_rst
+add wave -noupdate -expand -group Inputs -label Clear /tb_flex_counter/tb_clear
+add wave -noupdate -expand -group Inputs -label Enable /tb_flex_counter/tb_count_enable
+add wave -noupdate -expand -group Inputs -label {Rollover Value} /tb_flex_counter/tb_rollover_val
+add wave -noupdate -expand -group Outputs -label {Count Out} /tb_flex_counter/tb_count_out
+add wave -noupdate -expand -group Outputs -label {Rollover Flag} /tb_flex_counter/tb_rollover_flag
+TreeUpdate [SetDefaultTree]
+WaveRestoreCursors {{Cursor 1} {0 ps} 0}
+quietly wave cursor active 0
+configure wave -namecolwidth 217
+configure wave -valuecolwidth 100
+configure wave -justifyvalue left
+configure wave -signalnamewidth 0
+configure wave -snapdistance 10
+configure wave -datasetprefix 0
+configure wave -rowmargin 4
+configure wave -childrowmargin 2
+configure wave -gridoffset 0
+configure wave -gridperiod 1
+configure wave -griddelta 40
+configure wave -timeline 0
+configure wave -timelineunits ns
+update
+WaveRestoreZoom {0 ps} {21958 ps}
