348e33a6de6764b88daf6821dcda8ef62a990a64
Dorien Penebacker <dpenebac@purdue.edu>
2022-02-28 09:54:09 -0500

    updated flex_counter from lab6 after copying from lab4

--- Lab7/source/flex_counter.sv
+++ Lab7/source/flex_counter.sv
@@ -4,8 +4,7 @@
 // Author:      Dorien Penebacker
 // Lab Section: 337-09
 // Version:     1.0  Initial Design Entry
-// Description: Flex Counter Lab 4
-//
+// Description: Flex Counter Lab 6
 
 `timescale 10ns/100ps
 
@@ -21,31 +20,44 @@ module flex_counter
 );
 
     logic [NUM_CNT_BITS - 1 : 0] next_count;
+    logic next_flag;
 
-    always_ff @(posedge clk, posedge clear, negedge n_rst) begin
-        if (clear == 1'b1)
-            count_out <= 0;
-        else if (n_rst == 1'b0)
+    always_ff @(posedge clk, negedge n_rst) begin
+        if (n_rst == 1'b0)
             count_out <= 0;
         else
             count_out <= next_count;
     end
 
-    always_comb begin
-        if (count_enable == 1'b1 && rollover_val == count_out)
-            next_count = 1'b1;
-        else if (count_enable == 1'b1)
-            next_count = count_out + 1;
-        else 
-            next_count = count_out;
+    always_ff @(posedge clk, negedge n_rst) begin
+        if (n_rst == 1'b0)
+            rollover_flag <= 0;
+        else
+            rollover_flag <= next_flag;
     end
 
     always_comb begin
-        if (rollover_val == count_out) begin
-            rollover_flag = 1'b1;
+        next_count = count_out;
+        next_flag = rollover_flag;
+
+        if (clear) begin
+            next_flag = 1'b0;
+            next_count = 1'b0;
         end
-        else begin
-            rollover_flag = 1'b0;
+
+        if (count_enable && (count_out == (rollover_val - 1))) begin
+            next_flag = 1'b1;
+            next_count = count_out + 1;
+        end
+
+        else if (count_enable && (count_out >= rollover_val)) begin
+            next_count = 1;
+            next_flag = 1'b0;
+        end
+
+        else if (count_enable) begin
+            next_count = count_out + 1;
+            next_flag = 1'b0;
         end
     end
 
